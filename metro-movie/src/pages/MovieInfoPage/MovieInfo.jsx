4237b8cb39cbf379bcad924a89520d945914a086
andreinarivas <113389360+andreinarivas@users.noreply.github.com>
2023-06-11 09:31:24 -0400

    reservas

--- metro-movie/src/pages/MovieInfoPage/MovieInfo.jsx
+++ metro-movie/src/pages/MovieInfoPage/MovieInfo.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import styles from './MovieInfo.module.css'
+import React from "react";
+import styles from "./MovieInfo.module.css";
 import Button from "../../components/Button/Button";
 
-
-
 export default function MovieInfo() {
-
   return (
     <div className={styles.container}>
-    <div className={styles.wrapper}>
-      <div className={styles.card}>
-        <div className={styles.card_back}>
+      <div className={styles.wrapper}>
+        <div className={styles.card}>
           <img
             className={styles.pic}
             src="https://render.fineartamerica.com/images/rendered/default/poster/8/10/break/images/artworkimages/medium/3/almost-famous-bo-kev.jpg"
           />
-        </div>
-        <div className={styles.card_front}>
+
           <h4>TITULO</h4>
           <div>
             <h6>Idiomas:</h6>
@@ -35,22 +30,27 @@ export default function MovieInfo() {
             <p></p>
           </div>
         </div>
+
+        <div className={styles.infomovie}>
+          <h1 className={styles.title}>TITULO DE LA PELICULA (AÑO)</h1>
+          <h2>Sinopsis</h2>
+          <p>
+            Lorem ipsum dolor sit amet , consectetur adipiscing elit. Vivamus id
+            velit efficitur, mattis quam nec, vehicula odio. Proin ut elit sit
+            amet lectus laoreet placerat. Fusce sit amet cursus nulla, non
+            malesuada arcu. Curabitur posuere ipsum ut gravida imperdiet.
+            Maecenas aliquam ut purus vel sollicitudin. Aenean malesuada leo
+            nunc, sed rutrum velit lacinia vel. Nam sollicitudin sapien nec
+            justo iaculis fermentum. Aliquam consequat, orci ut molestie
+            bibendum, ipsum lorem feugiat odio, eu maximus tellus enim vitae ex.
+          </p>
+          <h4>Actores:</h4>
+          <h4>Idiomas:</h4>
+          <h4>Duracion:</h4>
+          <h4>Genero:</h4>
+        </div>
+        <Button display="Reservar" />
       </div>
-      <div className= {styles.infomovie}>
-        <h1 className={styles.title}>TITULO DE LA PELICULA (AÑO)</h1>
-        <h2>Sinopsis</h2>
-        <p>Lorem ipsum dolor sit amet , consectetur adipiscing elit. Vivamus id velit efficitur, mattis quam nec, vehicula odio. Proin ut elit sit amet lectus laoreet placerat. Fusce sit amet cursus nulla, non malesuada arcu. Curabitur posuere ipsum ut gravida imperdiet. Maecenas aliquam ut purus vel sollicitudin. Aenean malesuada leo nunc, sed rutrum velit lacinia vel. Nam sollicitudin sapien nec justo iaculis fermentum. Aliquam consequat, orci ut molestie bibendum, ipsum lorem feugiat odio, eu maximus tellus enim vitae ex.</p>
-        <h4>Actores:</h4>
-        <h4>Idiomas:</h4>
-        <h4>Duracion:</h4>
-        <h4>Genero:</h4>
-      </div>
-      <Button display="Reservar" />
-
     </div>
-
-
-
-    </div>
-  )
+  );
 }
